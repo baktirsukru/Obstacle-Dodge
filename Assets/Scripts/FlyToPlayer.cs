854bb784fc0f2cc31a958b7f5ba7ca43b26cc2c7
baktirsukru <101118829+baktirsukru@users.noreply.github.com>
2024-11-05 13:02:20 +0300

    projectile_follows_player

--- Assets/Scripts/FlyToPlayer.cs
+++ Assets/Scripts/FlyToPlayer.cs
@@ -11,12 +11,13 @@ public class FlyToPlayer : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerPosition = player.transform.position;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        playerPosition = player.transform.position;
         transform.position = 
         Vector3.MoveTowards(transform.position, playerPosition, (Time.deltaTime * moveSpeed));
     }
